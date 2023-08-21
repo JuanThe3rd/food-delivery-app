3836ab6a230899cfeee19d7e5f21904b3cc91bf7
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-08-21 00:45:39 -0700

    completes registration page before testing

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,123 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function Register() {
-    return <h1>Register Page</h1>;
+    const [accountType, setAccountType] = useState();
+    const [accountData, setAccountData] = useState({});
+    const [newAccount, setNewAccount] = useState();
+    const [msg, setMsg] = useState();
+
+    return (
+        <div>
+            <h1>Registration Page</h1>
+
+            {msg && 
+                <div>
+                    {msg}
+                </div>
+            }
+
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input placeholder='Username' onChange={handleChange} name='username' />
+                <br />
+                <input placeholder='Password' onChange={handleChange} name='password' type='password' />
+                <br />
+                <label>Account Type: </label>
+                <select onChange={(e) => setAccountType(e.target.value)}>
+                    <option value='choosing'>-- Select --</option>
+                    <option value='user'>User</option>
+                    <option value='restaurant'>Restaurant</option>
+                </select>
+                <br />
+
+                {accountType === 'user' && 
+                    <div>
+                        <input placeholder='Firstname' name='firstname' onChange={handleChange} />
+                        <br/ >
+                        <input placeholder='Profile Picture URL' name='profile_pic' onChange={handleChange} />
+                        <br />
+                    </div>
+                }
+
+                {accountType === 'restaurant' &&
+                    <div>
+                        <input placeholder='Restaurant Name' onChange={handleChange} name='name' value={accountData.name} />
+                        <br />
+                        <input placeholder='Image URL' onChange={handleChange} name='image' value={accountData.image} />
+                        <br />
+                        <input placeholder='Owner Name' onChange={handleChange} name='owner' value={accountData.owner} />
+                        <br />
+                    </div>
+                }
+
+                <input type='submit' />
+            </form>
+        </div>
+    );
+
+    function handleChange(e){
+        setAccountData({...accountData, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        if (accountType === 'user'){
+            fetch('/users', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name: accountData.firstname,
+                    profile_pic: accountData.profile_pic
+                })
+            })
+                .then(res => res.json())
+                .then(setNewAccount)
+                .catch(setMsg)
+
+            fetch('/logins', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    username: accountData.username,
+                    password: accountData.password,
+                    user_type: 'user',
+                    user_id: newAccount.id,
+                    restaurant_id: null
+                })
+            })
+                .then(res => res.json())
+                .then(newLogin => setMsg('Account Successfully Created!'))
+                .catch(setMsg)
+
+        } else if (accountType ==='restaurant'){
+            fetch('/restaurants', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name: accountData.name,
+                    image: accountData.image,
+                    owner: accountData.owner
+                })
+            })
+                .then(res => res.json())
+                .then(setNewAccount)
+                .catch(setMsg)
+
+            fetch('/logins', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    username: accountData.username,
+                    password: accountData.password,
+                    user_type: 'restaurant',
+                    user_id: null,
+                    restaurant_id: newAccount.id
+                })
+            })
+                .then(res => res.json())
+                .then(newLogin => setMsg('Account Successfully Created!'))
+        }
+    }
 }
 
 export default Register;
